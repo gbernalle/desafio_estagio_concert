85530eaedd8aea6d64be738515461c3f374fc8c3
gbernalle <gabrielbbmatozinhos@gmail.com>
2025-07-10 14:40:10 -0300

    Rotas do backend criadas sem validação, deve-se analisar possíveis gambiarras

--- backend/src/machine/machine.controller.ts
+++ backend/src/machine/machine.controller.ts
@@ -1,30 +1,40 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { MachineService } from './machine.service';
 
 @Controller('machine') //  /machine
 export class MachineController {
 
-  @Get() // GET /machine
-  findAll() {
-    return []
+  constructor(private readonly machineService: MachineService) {}
+
+  @Get() // GET /machine or /machine?status=value
+  findAll(@Query('status') status?:'Operando' | 'Parada para manutencao' | 'Desligada') {
+    return this.machineService.findAll(status)
   }
 
   @Get(':id')  // GET /machine/:id
   findOne(@Param('id') id: string) {
-    return {id}
+    return this.machineService.findOne(+id)
   }
 
   @Post() // POST /machine
-  create(@Body() machine: {}) {
-    return machine
+  create(@Body() machine: {
+    name: string,
+    email: string,
+    location: string,
+    status: 'Operando' | 'Parada para manutencao' | 'Desligada'}) {
+    return this.machineService.create(machine)
   }
 
   @Patch(':id')  // PATCH /machine/:id
-  update(@Param('id') id: string, @Body() machineUpdate: {}) {
-    return {id, ...machineUpdate}
+  update(@Param('id') id: string, @Body() machineUpdate: {
+    location: string,
+    status: 'Operando' | 'Parada para manutencao' | 'Desligada'
+  }) {
+    return this.machineService.update(+id,machineUpdate)
   }
 
   @Delete(':id') // DELETE /machine/:id
   delete(@Param('id') id: string) {
-    return {id}
+    return this.machineService.delete(+id)
   }
 }
--- backend/src/machine/machine.service.ts
+++ backend/src/machine/machine.service.ts
@@ -1,4 +1,83 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class MachineService {}
+export class MachineService {
+  private machine = [
+    {
+      "id": 1,
+      "name": "firstMachine",
+	    "location": "Belo Horizonte MG, BR",
+	    "status": "Operando"	
+    },
+    {
+      "id": 2,
+      "name": "secondMachine",
+	    "location": "Belo Horizonte MG, BR",
+	    "status": "Operando"	
+    },
+    {
+      "id": 3,
+      "name": "thirdMachine",
+	    "location": "Belo Horizonte MG, BR",
+	    "status": "Operando"	
+    },
+    {
+      "id": 4,
+      "name": "fourthMachine",
+	    "location": "Belo Horizonte MG, BR",
+	    "status": "Operando"	
+    }
+  ]
+
+  findAll(status?: 'Operando' | 'Parada para manutencao' | 'Desligada') {
+    if (status) {
+      return this.machine.filter(machine => machine.status === status)
+    }
+    return this.machine
+  }
+
+  findOne(id: number) {
+    const mach = this.machine.find(machine => machine.id === id)
+
+    return mach
+  }
+
+  create(machine: {
+    name: string,
+    email: string,
+    location: string,
+    status: 'Operando' | 'Parada para manutencao' | 'Desligada'
+  }) {
+    const machineByHighestId = [...this.machine].sort((a, b) => b.id - a.id)
+    const newMachine = {
+      id: machineByHighestId[0].id + 1,
+      ...machine
+    }
+    this.machine.push(newMachine)
+    return newMachine
+  }
+
+  update(id: number,
+    updateMachine: {
+      location: string,
+      status: 'Operando' | 'Parada para manutencao' | 'Desligada'
+    }
+  ) {
+    this.machine = this.machine.map(machine => {
+      if (machine.id === id) {
+        return {...machine, ...updateMachine}
+      }
+      return machine
+    })
+    return this.findOne(id)
+  }
+
+  delete(id: number) {
+    const removeMachine = this.findOne(id)
+
+    this.machine = this.machine.filter(machine => machine.id !== id)
+
+    return removeMachine
+  }
+}
