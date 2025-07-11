47ef53296603ec2398df2b749aba30e93e2a80c6
gbernalle <gabrielbbmatozinhos@gmail.com>
2025-07-11 14:03:41 -0300

    Fim da criação da migration. É preciso analisar como configurar para ir para o servidor da Neon

---
+++ backend/prisma/migrations/20250710203929_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('Operando', 'Parada_para_manutenção', 'Desligada');
+
+-- CreateTable
+CREATE TABLE "Machine" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Machine_pkey" PRIMARY KEY ("id")
+);
